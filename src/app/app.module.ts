dd02f6eb098bbac550dac08e7ffa21e5b30a8272
khiemdc <khiemle@verizon.net>
2018-04-05 02:43:58 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
+    MDBBootstrapModule,
     AppRoutingModule
   ],
+  schemas: [ NO_ERRORS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
